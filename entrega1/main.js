18b929e15b437713c089262c222bbb249ed22dc9
Agustin Dieux <agusdieux@hotmail.com>
2022-08-29 20:27:38 -0300

    34020-AgustinDieux-PrimerDesafio

---
+++ entrega1/main.js
@@ -0,0 +1,6 @@
+console.log("Bienvenido al programa")
+
+let cantidad=prompt("ingrese la cantidad de veces que quiera que se repita el mensaje")
+for (var i = 1; i<=cantidad; i++) {
+    console.log("hola" +i)
+}
